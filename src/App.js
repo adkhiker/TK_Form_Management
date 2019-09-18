f920608b783dc4481736f0c90fdb6ffb177c8e9f
adkhiker <poirier.greg@gmail.com>
2019-09-18 09:17:29 -0400

    logs state to console without refreshing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handleChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(name);
+  };
+
   return (
     <div className="App">
-      <form>
+      {console.log(name)}
+      <form onSubmit={event => handleSubmit(event)}>
         <label>
           Username:
-          <input type="text" />
+          <input type="text" onChange={event => handleChange(event)} />
         </label>
+        <button>Submit!</button>
       </form>
     </div>
   );
