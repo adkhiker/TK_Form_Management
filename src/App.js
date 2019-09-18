3c50ff1b04e4b1f1a2b7085144fbd9052bcfffed
adkhiker <poirier.greg@gmail.com>
2019-09-18 10:28:38 -0400

    End of TK

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
+  const [user, setUser] = useState({ username: "", password: "" });
 
   const handleChange = event => {
-    setName(event.target.value);
+    setUser({ ...user, [event.target.name]: event.target.value });
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(name);
+    console.log(user.name);
+    console.log(user.password);
   };
 
   return (
     <div className="App">
-      {console.log(name)}
+      {console.log(user)}
       <form onSubmit={event => handleSubmit(event)}>
         <label>
           Username:
-          <input type="text" onChange={event => handleChange(event)} />
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="text"
+            name="password"
+            value={user.password}
+            onChange={event => handleChange(event)}
+          />
         </label>
         <button>Submit!</button>
       </form>
