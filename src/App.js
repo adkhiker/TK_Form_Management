ee644bb9c479f2349e594ae3ccd85c5e6b44442c
adkhiker <poirier.greg@gmail.com>
2019-09-19 10:44:55 -0400

    final commit for the day

--- src/App.js
+++ src/App.js
@@ -1,43 +1,68 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-function LoginForm() {
-  const [user, setUser] = useState({ username: "", password: "" });
-
-  const handleChange = event => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(user.username);
-    console.log(user.password);
-  };
-
+function LoginForm({ values, errors, touched, isSubmitting }) {
   return (
-    <div className="loginForm">
-      {console.log(user)}
-      <form onSubmit={event => handleSubmit(event)}>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="text"
-            name="password"
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <button>Submit!</button>
-      </form>
-    </div>
+    <Form>
+      <div>
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+      <label>
+        <Field type="checkbox" name="tos" checked={values.tos} />
+        Accept TOS
+      </label>
+      <Field component="select" name="meal">
+        <option value="gold">Gold</option>
+        <option value="silver">Silver</option>
+        <option value="platinum">Platinum</option>
+      </Field>
+      <button disabled={isSubmitting}>Submit</button>
+    </Form>
   );
 }
 
-export default LoginForm;
+const FormikLoginForm = withFormik({
+  mapPropsToValues({ email, password, tos, meal }) {
+    return {
+      email: email || "",
+      password: password || "",
+      tos: tos || false,
+      meal: meal || "silver"
+    };
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(16, "Password must be 16 characters or longer")
+      .required("Password is required")
+  }),
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+    if (values.email === "alreadytaken@atb.dev") {
+      setErrors({ email: "That email is already taken" });
+    } else {
+      axios
+        .post("https://yourdatabaseurlgoeshere.com", values)
+        .then(res => {
+          console.log(res); // Data was created successfully and logs to console
+          resetForm();
+          setSubmitting(false);
+        })
+        .catch(err => {
+          console.log(err); // There was an error creating the data and logs to console
+          setSubmitting(false);
+        });
+    }
+  }
+})(LoginForm);
+
+export default FormikLoginForm;
